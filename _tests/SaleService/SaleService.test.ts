92fadd24504f2259fc5cf3d745f998082268d3c6
liamorourkemorgan <liamorourkemorgan@gmail.com>
2020-06-26 09:57:13 +0100

    Cleaning up

--- _tests/SaleService/SaleService.test.ts
+++ _tests/SaleService/SaleService.test.ts
@@ -7,14 +7,6 @@ import InventoryService from '../../services/InventoryService'
 import StockService from '../../services/StockService'
 import BankService from '../../services/BankService'
 
-import { calculateBalance } from 'helpers/coinHelper'
-
-/*
-  SaleService:
-  - Should be able to check whether USER_COINS is greater than or equal to REQUIRED_COINS.
-  - Should be able to calculate change
-*/
-
 describe('Sale Service', () => {
   it('Should be able to check whether userCoins is greater than or equal to requiredCoins', () => {
     const coinsToAdd: Array<Coin> = [new Coin(5.6, 2.4), new Coin(5.6, 2.4)]
